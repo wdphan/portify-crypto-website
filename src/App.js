277806294e3a82e6c19866380c5a7236e3edf052
William Phan <willdphan@gmail.com>
2022-05-21 15:18:17 -0400

    added home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom'
 import Notification from './components/Notification'
-import Header from './Header'
-import Prices from './CoinPrices'
+import Header from './Header.jsx'
+import CoinPrices from './CoinPrices'
 import Coin from './Coin'
+import Home from './Home'
 
 function App() {
   return (
@@ -12,8 +13,11 @@ function App() {
       <Header/>
     <div>
         <Switch>
-          <Route path="/CoinPrices">
-         <Prices/>
+          <Route path="/" exact component={Home}>
+         <Home/>
+         </Route>
+          <Route path="/CoinPrices" exact component={CoinPrices}>
+         <CoinPrices/>
          </Route>
           <Route path="/Coin">
          <Coin/>
--- src/components/CoinChart.jsx
+++ src/components/CoinChart.jsx
@@ -1,11 +1,67 @@
 import React from "react";
-import {useEffect, useState} from 'react'
-import axios from 'axios'
+import { Line } from 'react-chartjs-2'
+import Chart from 'chart.js/auto'
 
-const CoinChart = () => {
+const data = {
+  labels: [
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+    'Jan',
+    'Feb',
+    'Mar',
+    'May',
+    'Jun',
+  ],
+  datasets: [
+    {
+      fill: true,
+      backgroundColor: '#010180',
+      drawOnChartArea: false,
+      display: false,
+      lineTension: .5,
+      backgroundColor: '#D3D3EB',
+      borderColor: '#010180',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: '#010180',
+      pointBackgroundColor: '#010180',
+      pointBorderWidth: 1,
+      pointHoverRadius: 7,
+      pointHoverBackgroundColor: '#010180',
+      pointHoverBorderColor: '#010180',
+      pointHoverBorderWidth: 2,
+      pointRadius: 0,
+      pointHitRadius: 10,
+      data: [3000, 3200, 3400, 3500, 3400, 3300, 3450, 3500, 3500, 3500, 3700, 3800, 3900, 3700, 3300, 3000, 2900, 2600, 2300],  
+        },
+  ],
+}
+
+
+
+const options = {
+  plugins: {
+    legend: {
+      display: false,
+    },
+    
+  },
+}
+
+const BalanceChart = () => {
   return (
-    <div>CoinChart</div>
+      <Line data={data} options={options} width={400} height={200} />
   )
 }
 
-export default CoinChart
+export default BalanceChart
+
